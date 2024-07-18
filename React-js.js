ffd3808466a65745776ffbde0e0357df9be86a03
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-18 20:17:39 +0600

    add code

--- React-js.js
+++ React-js.js
@@ -1,7 +1,15 @@
 let arr = [1, 2, 3];
 
-function sum (a, b, c) {
-    // console.log(a,b,c)
+let obj ={
+    name: 'Sweety',
+    age:124,
+}
+
+function sum () {
+    // console.log({arguments});
+
+    Object.values(arguments).map(val=>
+        console.log(val));
     return 5;
 }
 console.log(sum(2, 3));
